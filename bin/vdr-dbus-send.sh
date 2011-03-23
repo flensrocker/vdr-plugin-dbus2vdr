b64c6b44db349622b5ba908e61df66d695da4ebf
Lars Hanisch <dvb@flensrocker.de>
2011-03-23 19:03:07 +0100

    add helper script for calling dbus2vdr

---
+++ bin/vdr-dbus-send.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]
+then
+  echo "usage: $0 objectpath interface.method [arguments]"
+  exit 1
+fi
+
+DEST=de.tvdr.vdr
+OBJECT=$1
+shift
+INTERFACE=$1
+shift
+
+dbus-send --system --type=method_call --dest=$DEST --print-reply $OBJECT $DEST.$INTERFACE $*
