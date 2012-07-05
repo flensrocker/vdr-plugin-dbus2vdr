15427afdb83c409d6fd905069b602ad736ab8147
Lars Hanisch <dvb@flensrocker.de>
2012-07-05 18:09:34 +0200

    fix a memory leak and a bug in setting generic values in setup.Set

--- dbus2vdr.c
+++ dbus2vdr.c
@@ -24,7 +24,7 @@
 #include <vdr/osdbase.h>
 #include <vdr/plugin.h>
 
-static const char *VERSION        = "0.0.7e";
+static const char *VERSION        = "0.0.7f";
 static const char *DESCRIPTION    = trNOOP("control vdr via D-Bus");
 static const char *MAINMENUENTRY  = NULL;
 
