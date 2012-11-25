a748984469b7283ab4710ca4e7242298f8cf12e3
Lars Hanisch <dvb@flensrocker.de>
2012-11-25 12:12:55 +0100

    refactored message handling so extending is easier

--- recording.h
+++ recording.h
@@ -4,25 +4,6 @@
 #include "message.h"
 
 
-class cDBusMessageRecording : public cDBusMessage
-{
-friend class cDBusDispatcherRecording;
-
-public:
-  enum eAction { dmrUpdate };
-
-  virtual ~cDBusMessageRecording(void);
-
-protected:
-  virtual void Process(void);
-
-private:
-  cDBusMessageRecording(eAction action, DBusConnection* conn, DBusMessage* msg);
-  void Update(void);
-
-  eAction _action;
-};
-
 class cDBusDispatcherRecording : public cDBusMessageDispatcher
 {
 public:
@@ -30,7 +11,6 @@ public:
   virtual ~cDBusDispatcherRecording(void);
 
 protected:
-  virtual cDBusMessage *CreateMessage(DBusConnection* conn, DBusMessage* msg);
   virtual bool          OnIntrospect(DBusMessage *msg, cString &Data);
 };
 
