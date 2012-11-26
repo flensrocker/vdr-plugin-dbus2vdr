4752e49339266fc69d588ae2dbc2f8cc66ec0828
Lars Hanisch <dvb@flensrocker.de>
2012-11-26 20:55:18 +0100

    handle arguments with type variant in the right way

--- recording.c
+++ recording.c
@@ -66,15 +66,19 @@ public:
     cRecording *recording = NULL;
     DBusMessageIter args;
     if (dbus_message_iter_init(msg, &args)) {
-       if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_STRING) {
-          const char *path = NULL;
-          if ((cDBusHelper::GetNextArg(args, DBUS_TYPE_STRING, &path) >= 0) && (path != NULL) && *path)
-             recording = recordings.GetByName(path);
-          }
-       else if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_INT32) {
-          int number = 0;
-          if ((cDBusHelper::GetNextArg(args, DBUS_TYPE_INT32, &number) >= 0) && (number > 0) && (number <= recordings.Count()))
-             recording = recordings.Get(number - 1);
+       if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_VARIANT) {
+          DBusMessageIter sub;
+          dbus_message_iter_recurse(&args, &sub);
+          if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_STRING) {
+             const char *path = NULL;
+             if ((cDBusHelper::GetNextArg(sub, DBUS_TYPE_STRING, &path) >= 0) && (path != NULL) && *path)
+                recording = recordings.GetByName(path);
+             }
+          else if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_INT32) {
+             int number = 0;
+             if ((cDBusHelper::GetNextArg(sub, DBUS_TYPE_INT32, &number) >= 0) && (number > 0) && (number <= recordings.Count()))
+                recording = recordings.Get(number - 1);
+             }
           }
        }
     DBusMessage *reply = dbus_message_new_method_return(msg);
@@ -102,43 +106,69 @@ public:
     cDBusHelper::SendReply(conn, msg, reply);
   };
 
-  static void PlayPath(DBusConnection* conn, DBusMessage* msg)
+  static void Play(DBusConnection* conn, DBusMessage* msg)
   {
+    int replyCode = 501;
+    cString replyMessage;
     cRecording *recording = NULL;
-    const char *path = NULL;
     int position = -1; // default: resume
     const char *hmsf = NULL;
     DBusMessageIter args;
     if (dbus_message_iter_init(msg, &args)) {
-       if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_STRING) {
-          if ((cDBusHelper::GetNextArg(args, DBUS_TYPE_STRING, &path) >= 0) && (path != NULL) && *path) {
-             recording = recordings.GetByName(path);
-             if (recording == NULL) {
-                recordings.Update(true);
+       if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_VARIANT) {
+          DBusMessageIter sub;
+          dbus_message_iter_recurse(&args, &sub);
+          if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_STRING) {
+             const char *path = NULL;
+             if ((cDBusHelper::GetNextArg(sub, DBUS_TYPE_STRING, &path) >= 0) && (path != NULL) && *path) {
                 recording = recordings.GetByName(path);
-                }
-             if (recording != NULL) {
-                if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_STRING) {
-                   if ((cDBusHelper::GetNextArg(args, DBUS_TYPE_STRING, &hmsf) >= 0) && (hmsf != NULL) && *hmsf) {
-                      if (strcasecmp(hmsf, "begin") == 0) {
-                         position = 0;
-                         hmsf = NULL;
-                         }
-                      else
-                         position = HMSFToIndex(hmsf, recording->FramesPerSecond());
-                      }
-                   else
-                      hmsf = NULL;
+                if (recording == NULL) {
+                   recordings.Update(true);
+                   recording = recordings.GetByName(path);
+                   if (recording == NULL)
+                      replyMessage = cString::sprintf("recording \"%s\" not found", path);
                    }
-                else if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_INT32)
-                   cDBusHelper::GetNextArg(args, DBUS_TYPE_INT32, &position);
+                }
+             }
+          else if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_INT32) {
+             int number = 0;
+             if ((cDBusHelper::GetNextArg(sub, DBUS_TYPE_INT32, &number) >= 0) && (number > 0)) {
+                recording = recordings.Get(number - 1);
+                if (recording == NULL) {
+                   recordings.Update(true);
+                   recording = recordings.Get(number - 1);
+                   if (recording == NULL)
+                      replyMessage = cString::sprintf("recording \"%d\" not found", number);
+                   }
                 }
              }
           }
+
+       if (recording != NULL) {
+          dbus_message_iter_next(&args);
+          if (dbus_message_iter_get_arg_type(&args) == DBUS_TYPE_VARIANT) {
+             DBusMessageIter sub;
+             dbus_message_iter_recurse(&args, &sub);
+             if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_STRING) {
+                if ((cDBusHelper::GetNextArg(sub, DBUS_TYPE_STRING, &hmsf) >= 0) && (hmsf != NULL) && *hmsf) {
+                   if (strcasecmp(hmsf, "begin") == 0) {
+                      position = 0;
+                      hmsf = NULL;
+                      }
+                   else
+                      position = HMSFToIndex(hmsf, recording->FramesPerSecond());
+                   }
+                else
+                   hmsf = NULL;
+                }
+             else if (dbus_message_iter_get_arg_type(&sub) == DBUS_TYPE_INT32)
+                cDBusHelper::GetNextArg(sub, DBUS_TYPE_INT32, &position);
+             }
+          }
        }
-    int replyCode = 501;
-    cString replyMessage;
+
     if (recording != NULL) {
+       replyCode = 250;
        cReplayControl::SetRecording(NULL);
        cControl::Shutdown();
        if (position >= 0) {
@@ -153,14 +183,12 @@ public:
        cControl::Attach();
 
        if (hmsf != NULL)
-          replyMessage = cString::sprintf("Playing recording \"%s\" from position %s", path, hmsf);
+          replyMessage = cString::sprintf("Playing recording \"%s\" from position %s", recording->FileName(), hmsf);
        else if (position >= 0)
-          replyMessage = cString::sprintf("Playing recording \"%s\" from position %d", path, position);
+          replyMessage = cString::sprintf("Playing recording \"%s\" from position %d", recording->FileName(), position);
        else
-          replyMessage = cString::sprintf("Resume playing recording \"%s\"", path);
+          replyMessage = cString::sprintf("Resume playing recording \"%s\"", recording->FileName());
        }
-    else
-       replyMessage = cString::sprintf("recording \"%s\" not found", path);
     cDBusHelper::SendReply(conn, msg, replyCode, *replyMessage);
   };
 };
@@ -176,7 +204,7 @@ cDBusDispatcherRecording::cDBusDispatcherRecording(void)
   AddAction("Update", cDBusRecordingActions::Update);
   AddAction("Get", cDBusRecordingActions::Get);
   AddAction("List", cDBusRecordingActions::List);
-  AddAction("PlayPath", cDBusRecordingActions::PlayPath);
+  AddAction("Play", cDBusRecordingActions::Play);
 }
 
 cDBusDispatcherRecording::~cDBusDispatcherRecording(void)
@@ -198,17 +226,17 @@ bool          cDBusDispatcherRecording::OnIntrospect(DBusMessage *msg, cString &
   "      <arg name=\"replymessage\" type=\"s\" direction=\"out\"/>\n"
   "    </method>\n"
   "    <method name=\"Get\">\n"
-  "      <arg name=\"option\"       type=\"i\" direction=\"in\"/>\n"
-  "      <arg name=\"recording\"    type=\"(ia(sv))\" direction=\"out\"/>\n"
+  "      <arg name=\"number_or_path\" type=\"v\" direction=\"in\"/>\n"
+  "      <arg name=\"recording\"      type=\"(ia(sv))\" direction=\"out\"/>\n"
   "    </method>\n"
   "    <method name=\"List\">\n"
   "      <arg name=\"recordings\"   type=\"a(ia(sv))\" direction=\"out\"/>\n"
   "    </method>\n"
-  "    <method name=\"PlayPath\">\n"
-  "      <arg name=\"path\"         type=\"s\" direction=\"in\"/>\n"
-  "      <arg name=\"begin\"        type=\"v\" direction=\"in\"/>\n"
-  "      <arg name=\"replycode\"    type=\"i\" direction=\"out\"/>\n"
-  "      <arg name=\"replymessage\" type=\"s\" direction=\"out\"/>\n"
+  "    <method name=\"Play\">\n"
+  "      <arg name=\"number_or_path\" type=\"v\" direction=\"in\"/>\n"
+  "      <arg name=\"begin\"          type=\"v\" direction=\"in\"/>\n"
+  "      <arg name=\"replycode\"      type=\"i\" direction=\"out\"/>\n"
+  "      <arg name=\"replymessage\"   type=\"s\" direction=\"out\"/>\n"
   "    </method>\n"
   "  </interface>\n"
   "</node>\n";
